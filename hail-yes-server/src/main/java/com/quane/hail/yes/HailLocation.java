32f7836a879aa8c08621a6ddce53b76a4a75370c
Sean Connolly <connolly.st@gmail.com>
2012-02-12 12:30:05 -0800

    Added location getter and setter support for e^6 coordinates (use in google maps).

--- hail-yes-server/src/main/java/com/quane/hail/yes/HailLocation.java
+++ hail-yes-server/src/main/java/com/quane/hail/yes/HailLocation.java
@@ -7,28 +7,46 @@ package com.quane.hail.yes;
  */
 public class HailLocation {
 
-	private Long latitude;
-	private Long longitude;
+	private static final int E6 = 1000000;
 
-	public HailLocation(Long latitude, Long longitude) {
+	private double latitude;
+	private double longitude;
+
+	public HailLocation(double latitude, double longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
 
-	public Long getLatitude() {
+	public double getLatitude() {
 		return latitude;
 	}
 
-	public void setLatitude(Long latitude) {
+	public void setLatitude(double latitude) {
 		this.latitude = latitude;
 	}
 
-	public Long getLongitude() {
+	public double getLongitude() {
 		return longitude;
 	}
 
-	public void setLongitude(Long longitude) {
+	public void setLongitude(double longitude) {
 		this.longitude = longitude;
+	}
+
+	public double getLatitudeE6() {
+		return (int) (latitude * E6);
+	}
+
+	public void setLatitudeE6(int latitudeE6) {
+		this.latitude = (latitudeE6 / E6);
+	}
+
+	public double getLongitudeE6() {
+		return (int) (longitude * E6);
+	}
+
+	public void setLongitudeE6(int longitudeE6) {
+		this.longitude = (longitudeE6 / E6);
 	}
 
 }
