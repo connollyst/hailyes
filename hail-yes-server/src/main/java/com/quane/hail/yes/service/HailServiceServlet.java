0fa646ad8614a505ba133a52dc7a4e6673a49c69
Sean Connolly <connolly.st@gmail.com>
2012-02-12 01:16:12 -0800

    Added log4j support

--- hail-yes-server/src/main/java/com/quane/hail/yes/service/HailServiceServlet.java
+++ hail-yes-server/src/main/java/com/quane/hail/yes/service/HailServiceServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import com.google.gson.Gson;
 import com.quane.hail.yes.HailUser;
 import com.quane.hail.yes.Location;
@@ -24,6 +26,8 @@ import com.quane.hail.yes.data.HailDAO;
 public class HailServiceServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 42L;
+
+	private Logger logger = Logger.getLogger(getClass());
 
 	/**
 	 * Default constructor.
@@ -45,17 +49,18 @@ public class HailServiceServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("doGet called");
+		logger.info("doGet called");
 		String jsonLocation = request.getParameter("location");
 		Gson gson = new Gson();
 		Location location = gson.fromJson(jsonLocation, Location.class);
 		List<HailUser> users = HailDAO.getUsersNearLocation(location);
 		String jsonUsers = gson.toJson(users);
+		logger.info("response: " + jsonUsers);
 		Writer writer = response.getWriter();
 		writer.write(jsonUsers);
 		writer.flush();
 		writer.close();
-		System.out.println("doGet done");
+		logger.info("doGet done");
 	}
 
 	/**
@@ -65,7 +70,7 @@ public class HailServiceServlet extends HttpServlet {
 	 */
 	protected void doPut(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("doPut called");
+		logger.info("doPut called");
 		// TODO create or update a cabbie or hailer
 	}
 
@@ -76,7 +81,7 @@ public class HailServiceServlet extends HttpServlet {
 	 */
 	protected void doDelete(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("doDelete called");
+		logger.info("doDelete called");
 		// TODO remove a cabbie or hailer
 	}
 
