94fff55f9546ee498faffe8db96e0a8b6ee67169
Sean Connolly <connolly.st@gmail.com>
2012-02-12 14:53:14 -0800

    Added hashCode and equals based on latitude & longitude

--- hail-yes-server/src/main/java/com/quane/hail/yes/HailLocation.java
+++ hail-yes-server/src/main/java/com/quane/hail/yes/HailLocation.java
@@ -49,4 +49,34 @@ public class HailLocation {
 		this.longitude = (longitudeE6 / E6);
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(latitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(longitude);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HailLocation other = (HailLocation) obj;
+		if (Double.doubleToLongBits(latitude) != Double
+				.doubleToLongBits(other.latitude))
+			return false;
+		if (Double.doubleToLongBits(longitude) != Double
+				.doubleToLongBits(other.longitude))
+			return false;
+		return true;
+	}
+
 }
