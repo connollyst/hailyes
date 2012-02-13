60d13fca056382dad5b8bf6193df2a61af0f457f
Sean Connolly <connolly.st@gmail.com>
2012-02-12 21:43:21 -0800

    Added a simple (probably poorly done) resource bundle to enforce consistency between core, server, and apps in things like WS parameter names and values.

---
+++ hail-yes-core/src/main/java/com/quane/hail/yes/StandardsResource.java
@@ -0,0 +1,13 @@
+package com.quane.hail.yes;
+
+public class StandardsResource {
+
+	public class QUERY_PARAMETER_NAMES {
+		public static final String COORDINATES_ARE_E6 = "e6";
+	}
+
+	public class QUERY_PARAMETER_VALUES {
+		public static final String COORDINATES_ARE_E6_TRUE = "1";
+		public static final String COORDINATES_ARE_E6_FALSE = "0";
+	}
+}
